15a84427f6a702a83997efa1e30f7e2f8ef2d13c
RishabhS05 <rishabhshri2795@gmail.com>
2023-07-10 15:21:02 +0530

    Initial Commit

---
+++ Samvadanam/SamvadanamApp.swift
@@ -0,0 +1,17 @@
+//
+//  SamvadanamApp.swift
+//  Samvadanam
+//
+//  Created by Rishabh Shrivastava on 10/07/23.
+//
+
+import SwiftUI
+
+@main
+struct SamvadanamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
